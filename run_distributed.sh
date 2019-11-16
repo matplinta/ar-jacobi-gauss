fc1cab33f4063edf12f7c49d00089d7209c3454f
Mateusz Plinta <matplinta@gmail.com>
2019-11-16 19:30:53 +0100

    add charts

--- run_distributed.sh
+++ run_distributed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 ##Nazwa zlecenia
-#SBATCH -J Proj1AR
+#SBATCH -J Dist
 ## Liczba alokowanych węzłów
 #SBATCH -N 12
 ## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
@@ -8,14 +8,14 @@
 ## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 4GB na rdzeń)
 #SBATCH --mem-per-cpu=1GB
 ## Maksymalny czas trwania zlecenia (format HH:MM:SS)
-#SBATCH --time=20:0:0 
+#SBATCH --time=24:0:0 
 ## Nazwa grantu do rozliczenia zużycia zasobów
 ## Specyfikacja partycji
 #SBATCH -p plgrid
 ## Plik ze standardowym wyjściem
-#SBATCH --output="distributed.csv"
+#SBATCH --output="dist/%j.csv"
 ## Plik ze standardowym wyjściem błędów
-#SBATCH --error="dist_error.err"
+#SBATCH --error="dist/%j.err"
  
 module load plgrid/tools/chapel/1.20.0
 export  GASNET_PHYSMEM_MAX='128MB'
@@ -30,11 +30,18 @@ echo "method,iterations,problem,locales,tasks,tasksPerLocale,time"
 
 for n_nodes in {1..12}
 do
-    for i in 100 150 200
-    do
-        ./jacobi_distributed -nl $n_nodes --n=$i
-        ./gauss_distributed -nl $n_nodes --n=$i
-    done
+    ./jacobi_distributed -nl $n_nodes --n=100
+    ./gauss_distributed -nl $n_nodes --n=100
 done
 
+for n_nodes in {1..12}
+do
+    ./jacobi_distributed -nl $n_nodes --n=150
+    ./gauss_distributed -nl $n_nodes --n=150
+done
 
+for n_nodes in {1..12}
+do
+    ./jacobi_distributed -nl $n_nodes --n=200
+    ./gauss_distributed -nl $n_nodes --n=200
+done
--- run_shared.sh
+++ run_shared.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 ##Nazwa zlecenia
-#SBATCH -J Proj1AR
+#SBATCH -J Shared
 ## Liczba alokowanych węzłów
 #SBATCH -N 1
 ## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
@@ -8,14 +8,14 @@
 ## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 4GB na rdzeń)
 #SBATCH --mem-per-cpu=1GB
 ## Maksymalny czas trwania zlecenia (format HH:MM:SS)
-#SBATCH --time=3:0:0 
+#SBATCH --time=24:0:0 
 ## Nazwa grantu do rozliczenia zużycia zasobów
 ## Specyfikacja partycji
 #SBATCH -p plgrid
 ## Plik ze standardowym wyjściem
-#SBATCH --output="shared.csv"
+#SBATCH --output="shared/%j.csv"
 ## Plik ze standardowym wyjściem błędów
-#SBATCH --error="shared_error.err"
+#SBATCH --error="shared/%j.err"
  
 module load plgrid/tools/chapel/1.20.0
 export  GASNET_PHYSMEM_MAX='128MB'
@@ -30,12 +30,23 @@ echo "method,iterations,problem,locales,tasks,tasksPerLocale,time"
 
 for n_cores in {1..12}
 do
-    for i in 100 150 200
-    do
-        export CHPL_RT_NUM_THREADS_PER_LOCALE=$n_cores
-        ./jacobi_shared -nl 1 --n=$i
-        ./gauss_shared -nl 1 --n=$i
-    done
+    export CHPL_RT_NUM_THREADS_PER_LOCALE=$n_cores
+    ./jacobi_shared -nl 1 --n=100
+    ./gauss_shared -nl 1 --n=100
+done
+
+for n_cores in {1..12}
+do
+    export CHPL_RT_NUM_THREADS_PER_LOCALE=$n_cores
+    ./jacobi_shared -nl 1 --n=150
+    ./gauss_shared -nl 1 --n=150
+done
+
+for n_cores in {1..12}
+do
+    export CHPL_RT_NUM_THREADS_PER_LOCALE=$n_cores
+    ./jacobi_shared -nl 1 --n=200
+    ./gauss_shared -nl 1 --n=200
 done
 
 
